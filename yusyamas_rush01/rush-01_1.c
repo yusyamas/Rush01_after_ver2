05ec9960e7e27bf99eab5a04e10165b40b3e19b6
yusyamas <yuppiy2000@icloud.com>
2022-10-15 14:01:40 +0900

    yusyamas_1

---
+++ yusyamas_rush01/rush-01_1.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush-01_1.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusyamas <yuppiy2000@icloud.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 09:10:42 by yusyamas          #+#    #+#             */
+/*   Updated: 2022/10/15 12:56:03 by yusyamas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+//-------------------------------------------
+//以上は使わない
+
+void	ft_swap(int *x, int i, int j)
+{
+	int	tmp;
+
+	tmp = x[i];
+	x[i] = x[j];
+	x[j] = tmp;
+}
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = size - 1;
+	while (i < size / 2)
+	{
+		ft_swap(tab, i, j);
+		i += 1;
+		j -= 1;
+	}
+}
+
+int	my_next_permutation(int *array, int num)
+{
+	int	i;
+	int	j;
+
+	i = num - 1;
+	while (array[i - 1] >= array[i])
+	{
+		if (--i == 0)
+		{
+			return (0);
+		}
+	}
+	j = num - 1;
+	while (j > i && array[j] <= array[i - 1])
+	{
+		j -= 1;
+	}
+	ft_swap(array, i - 1, j);
+	ft_rev_int_tab(array + i, num - i);
+	return (1);
+}
+
+//---------------------------------------------------
+//以下は使わない
+void	show_data(int *x, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d%c", x[i], (i != 3) ? ' ' : '\n');
+		i += 1;
+	}
+}
+
+int main(){
+	int x[4] = {1,2,3,4};
+	do {
+		show_data(x,4);
+	}while (my_next_permutation(x, 4));
+}
+
---
+++ yusyamas_rush01/rush-01_2.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush-01_2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusyamas <yuppiy2000@icloud.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 11:16:00 by yusyamas          #+#    #+#             */
+/*   Updated: 2022/10/15 13:36:41 by yusyamas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		my_next_permutation(int *array, int num);
+
+//条件を満たすなら*is_find = 1, そうでないなら*is_find = 0
+void	check_state(grid)
+{
+}
+
+void	grid_init(int *grid, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		grid[i] = i + 1;
+		i += 1;
+	}
+}
+
+void	grid_decision_dfs(int **grid, int depth, int *rule, int *is_find)
+{
+	if (depth == 4)
+	{
+		check_state(grid, rule, is_find);
+		return ;
+	}
+	grid_init(grid[depth], 4);
+	while (1)
+	{
+		grid_decision_dfs(grid, depth + 1, rule, is_find);
+		if (*is_find)
+		{
+			return ;
+		}
+		if (my_next_permutation(grid[depth], 4) == 0)
+		{
+			return ;
+		}
+	}
+}
